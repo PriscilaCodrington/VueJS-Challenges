1943a1473b17343ad4336cd8d665489e6d86b5a7
David Miguel <david.miguel@sngular.com>
2022-06-18 20:50:07 +0200

    Directive exercises

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import DirectiveSandbox from './components/DirectiveSandbox.vue'
+</script>
+
+<template>
+  <DirectiveSandbox />
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+</style>
---
+++ src/components/DirectiveSandbox.vue
@@ -0,0 +1,139 @@
+<script setup>
+import { ref } from 'vue'
+
+/** EXERCISE 1 VARIABLES **/
+const msg = "I am printing a text from the javascript.";
+
+/** EXERCISE 2 VARIABLES **/
+let booleanForExercise2 = true;
+
+/** EXERCISE 3 VARIABLES **/
+let numberForExercise3 = 5;
+
+/** EXERCISE 4 VARIABLES **/
+let arrayForExercise4 = [
+      {name: "Full-Stack developer", description: "Developing in both front and back sides"},
+      {name: "Front-end developer", description: "Specialized on developing in front side"},
+      {name: "Back-end developer", description: "Specialized on developing in back side"},
+      {name: "Data scientist", description: "Specialized on data treatment"},
+    ];
+
+/** EXERCISE 5 VARIABLES **/
+/**
+ * Variable for exercise 5. Notice that we are using 'ref' function, that allows
+ * to create a reactive variable, so when the value changes, is automatically refreshed
+ * and reflext the changes in template
+ * @type {Ref<UnwrapRef<{lastName: string, name: string, yearOfBirth: number}>>}
+ */
+let person = ref({
+  name: "David",
+  lastName: "Miguel de la Fuente",
+  yearOfBirth: 1989
+});
+
+/** EXERCISE 6 VARIABLES **/
+let counter = ref(0);
+function incrementCounter(){
+  return ++this.counter;
+}
+
+</script>
+
+<template>
+  <div>
+    <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125"/>
+    <h1>Directives in Vue</h1>
+  </div>
+  <div>
+    <p>Directives are reusable functionalities that you can bind to a component for many reasons. Also, you can
+      define custom ones, but this is not the point right now. There are several directives that is useful
+      to know that allow us to build enriched applications full of interactions in not so much time and easily.</p>
+    <p>Official documentation: <a href="https://vuejs.org/api/built-in-directives.html">Built-in directives</a></p>
+  </div>
+
+  <div class="exercise">
+    <h3>Exercise 1: Interpolation and v-text directive</h3>
+    <!-- TODO Exercise 1:
+    You can see that we have a variable that is returned inside an object to expose the value to the template.
+    That means you can use that variables inside the HTML, so you have to show that message using the both ways
+     that you need to know: using interpolation and using v-text directive-->
+  </div>
+
+  <div class="exercise">
+    <h3>Exercise 2: v-if directive</h3>
+    <!-- TODO Exercise 2:
+    We have a variable that is a boolean value, so we are going to use the v-if and v-else directives to load some
+    HTML elements or others. Add he directive to the following HTML elements and modify manually the value
+    of the variable to see the changes -->
+    <p>This paragraph should only be loaded if the value is "true"</p>
+    <p>If the conditions doesn't meet, we are going to show this paragraph</p>
+  </div>
+
+  <div class="exercise">
+    <h3>Exercise 3: v-show directive</h3>
+    <!-- TODO Exercise 3:
+    We are going to show the following HTML elements only if the numeric value meets certain condition -->
+    <p>The value at variable numberForExercise3 is positive</p>
+    <p>The value at variable numberForExercise3 is negative</p>
+    <p>The value at variable numberForExercise3 is 0</p>
+  </div>
+
+  <div class="exercise">
+    <h3>Exercise 4: v-for directive</h3>
+    <!-- TODO Exercise 4:
+    We have an array of objects. We want to iterate over the array and print certain HTML elements as many times
+    as elements we have. This time, we want to iterate over it and show something that fits the following example structure,
+    as many times as elements we have in the array to show a list of of job positions. The structure is:
+      Full-Stack developer: Developing in both front and back sides
+    You should use list (ordered or unordered) HTML elements.
+    -->
+  </div>
+
+  <div class="exercise">
+    <h3>Exercise 5: v-model directive</h3>
+    <!-- TODO Exercise 5:
+    We have a variable called "person", which is an JSON object. We are going to fix this brief form to edit
+    the information of the attributes in this object, so we can see live in the application how the values
+    on the variables change. This is quite powerful because allow us to bind values between inputs and
+    javascript, so we can execute extra functions with this data or allow the user to insert values that
+    can determine what kind os operations we have to execute.
+    -->
+    <p>Here, you can edit the person information. Check that the information in the bottom paragraph is refreshing live.</p>
+    <div>
+      Name: <input type="text" name="name" id="name">
+    </div>
+    <div>
+      Last name: <input type="text" name="lastName" id="lastName">
+    </div>
+    <div>
+      Year of birth: <input type="number" name="yearOfBirth" id="yearOfBirth">
+    </div>
+    <div>
+      <p><b>{{ person.name }} {{ person.lastName }} was born in {{ person.yearOfBirth }}</b></p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+
+.exercise {
+  border-top: solid 1px hsla(160, 100%, 37%, 1);
+  margin-top: 2rem;
+}
+</style>
